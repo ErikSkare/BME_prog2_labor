4765eede7e796edc7a25bb87cb2bc5d4e07a5856
ErikSkare <skareerik55@gmail.com>
2022-03-05 14:20:11 +0100

    lab_04 elofeladat

---
+++ lab_04/resistor.cpp
@@ -0,0 +1,42 @@
+﻿/**
+ * \file resistor.cpp
+ *   (UTF-8 kodolasu fajl. Allitsa at a megjenetes kodolasat,
+ *    ha a tovabbi kommentek nem olvashatok helyesen!)
+ *
+ * Ohmikus ellenállást modellező osztály megvalósítása
+ */
+
+/**
+ * Itt kell megvalósítani a resistor.h-ban deklarált nem inline függvényeket.
+ * A Jportára ezt a fájlt kell feltölteni.
+ */
+
+#include "resistor.h"
+
+double Resistor::defR = 76;
+
+Resistor::Resistor() {
+    this->R = defR;
+}
+
+Resistor::Resistor(double r) {
+    this->R = r;
+}
+
+void Resistor::setDef(double r) {
+    Resistor::defR = r;
+}
+
+Resistor Resistor::operator+(const Resistor& r) const {
+    return Resistor(this->R + r.getR());
+}
+
+Resistor Resistor::operator%(const Resistor& r) const {
+    return Resistor(1 / ((1 / this->R) + (1 / r.getR())));
+}
+
+Resistor operator*(int n, const Resistor& r) {
+    if(n <= 0)
+        throw "Z7ZF6D";
+    return Resistor(n * r.getR());
+}
